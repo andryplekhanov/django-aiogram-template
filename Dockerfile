9ba23397e96a9eaae8d94b102a81dea1e90aa243
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-08-04 11:09:12 +0300

    bot init

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
 FROM python:3.10-slim-buster
-WORKDIR /usr/src/app
 
+# переменные окружения для python
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-COPY requirements.txt .
+# устанавливаем зависимости
+RUN pip install --no-cache-dir --upgrade pip
+COPY ./requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
 
-RUN pip install --no-cache-dir --upgrade pip  \
-    && pip install --no-cache-dir -r ./requirements.txt
-
+# копируем содержимое текущей папки в контейнер
 COPY . .
---
+++ tgbot/config.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+
+from environs import Env
+
+
+@dataclass
+class DbConfig:
+    host: str
+    password: str
+    user: str
+    name: str
+    port: str
+
+
+@dataclass
+class TgBot:
+    token: str
+    admin_ids: list[int]
+    use_redis: bool
+
+
+@dataclass
+class Miscellaneous:
+    other_params: str = None
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    db: DbConfig
+    misc: Miscellaneous
+
+
+def load_config(path: str = None):
+    env = Env()
+    env.read_env(path)
+
+    return Config(
+        tg_bot=TgBot(
+            token=env.str("BOT_TOKEN"),
+            admin_ids=list(map(int, env.list("ADMINS"))),
+            use_redis=env.bool("USE_REDIS"),
+        ),
+        db=DbConfig(
+            host=env.str('POSTGRES_HOST'),
+            password=env.str('POSTGRES_PASSWORD'),
+            user=env.str('POSTGRES_USER'),
+            name=env.str('POSTGRES_DB'),
+            port=env.str('POSTGRES_PORT')
+        ),
+        misc=Miscellaneous()
+    )
